417a0c986de490d16d38f8f41f119976c10f781b
Marcel-Goncalves <marcel.goncalves.dev@outlook.com>
2023-07-20 12:37:54 -0300

    create exs

---
+++ aula012/ex012.js
@@ -0,0 +1,19 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora >= 1 && hora <= 5) {
+    console.log('Boa madrugada')
+}
+
+else if (hora > 5 && hora <12) {
+    console.log('Bom dia!')
+}
+
+else if (hora < 18) {
+    console.log('Boa Tarde!')
+}
+
+else {
+    console.log('Boa noite!')
+}
