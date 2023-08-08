b82a695444e338cccbb4e139b897727a967504dc
Marcel-Goncalves <marcel.goncalves.dev@outlook.com>
2023-08-08 00:01:32 -0300

    criação exercicio17

--- aula14Ex/ex017/script.js
+++ aula14Ex/ex017/script.js
@@ -1,1 +1,22 @@
-alert('ola')
+function calcular() {
+    let num1 = document.getElementById('entrada')
+    let tab = document.getElementById('seltab')
+    let res = document.getElementById('res')
+    
+    if (num1.value.length == 0) {
+        window.alert('Impossivel contar digite um valor')
+    }
+
+    else {
+        let contador = 0
+        let digitado = Number(num1.value)
+    tab.innerHTML = ''
+    while (contador <= 10){
+        let item = document.createElement('option')
+        item.text = `${digitado} x ${contador} = ${digitado*contador}`
+        item.value = `tab ${contador}`
+        tab.appendChild(item)
+        contador ++ }
+    }
+    
+}
