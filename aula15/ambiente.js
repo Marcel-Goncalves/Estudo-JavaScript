e0ab84a9f849b0cc29fff29fa899175473356274
Marcel-Goncalves <marcel.goncalves.dev@outlook.com>
2023-08-10 21:23:42 -0300

    update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,4 +1,6 @@
 let num = [5,8,2,9,3]
-
-console.log(`Nosso vetor é o ${num.length} posições`)
-console.log(num[3])
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[3]}`)
