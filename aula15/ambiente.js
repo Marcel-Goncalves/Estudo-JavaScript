ea3360fab3d74569b2dc4773aa7e08678563a72a
Marcel-Goncalves <marcel.goncalves.dev@outlook.com>
2023-08-13 21:41:02 -0300

    atualização js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,3 +4,10 @@ num.push(1)
 console.log(num)
 console.log(`Nosso vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[3]}`)
+let pos = num.indexOf(9)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado`)
+}
+else{
+console.log(`O valor está na posição ${pos}`)
+}
---
+++ aula16/função002.js
@@ -0,0 +1,5 @@
+function soma(n1=3, n2=5) {
+    return n1 + n2
+}
+
+console.log(soma(2))
